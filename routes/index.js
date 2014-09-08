ac589f71606ea3d35aaba03d24fb536850299aa3
Dan Pastusek <dan@hexial.com>
2014-09-08 11:44:23 -0600

    adding multiple images per step

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,57 @@
-var express = require('express');
-var fs = require('fs');
-var router = express.Router();
+var express = require('express'),
+    fs = require('fs'),
+    path = require('path'),
+    glob = require('glob'),
+    _ = require('lodash'),
+    router = express.Router();
+
 
 /* GET home page. */
-router.get('/:projectname', function (req, res) {
-    // projectname is set by the request's URL
-    var projectname = req.params.projectname;
+router.get('/:projectName', function (req, res) {
 
-    // set up pathname vars
-    var path = process.cwd() + "/public/projects/" + projectname;
-    var jsonpath = path + "/project.json";
+
+    // projectName is set by the request's URL
+    var projectName = req.params.projectName;
+
+    // set up projectPathname vars
+    var projectPath = process.cwd() + "/public/projects/" + projectName + "/";
+    var jsonProjectPath = projectPath + "project.json";
+
 
     // check if the JSON file exists.
-    if (fs.existsSync(jsonpath)) {
-        var projectdata = require(jsonpath);
-        console.log(projectdata);
-        res.render('project', {
-            name: projectname,
-            data: projectdata
-        });
+    if (fs.existsSync(jsonProjectPath)) {
+        var projectData = require(jsonProjectPath);
+        var finishedGlobbing = _.after(projectData.steps.length, doRender);
+        for (i=0; i<projectData.steps.length; i++){
+            //creating a closure to get the value of i after the glob finishes
+            (function (i) {
+                imgIndex = i+1;
+                glob(projectPath + "img/" + imgIndex + ".*.jpg", function (err, files){
+                    projectData.steps[i].thumbs = [];
+                    if (files.length > 1){
+                        for (j=0; j<files.length; j++){
+                            projectData.steps[i].thumbs.push(path.basename(files[j]));
+                        }
+                    }
+                    finishedGlobbing();
+                })
+            })(i);
+        }
     } else {
         res.send("404 - project does not exist");
     }
+
+    function doRender(){
+        res.render('project', {
+            name: projectName,
+            data: projectData
+        });
+    }
+});
+
+
+router.post('/api', function (req, res) {
+
 });
 
 module.exports = router;
